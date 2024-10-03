662cf64ab96e86ebec565a9f97ba882fdd13cbdd
AlbertRoca29 <albertrocallevadot@gmail.com>
2024-10-03 10:39:12 +0200

    few changes, a bit of a test

--- src/descriptors.py
+++ src/descriptors.py
@@ -3,11 +3,52 @@ from PIL.Image import Image
 
 
 class DescriptorGenerator:
-
     def compute(self, input_image: Image, bins: int = 256) -> np.array:
         pass
 
 class GreyScaleDescriptor(DescriptorGenerator):
     def compute(self, input_image: Image, bins: int = 256) -> np.array:
-        histogram, _ = np.histogram(input_image.convert('L'), bins=bins, range=(0, bins - 1))
+        histogram, _ = np.histogram(input_image.convert('L'), bins=bins, range=(0, bins))
         return histogram
+
+# TODO: Combine RGB and HSL descriptors to reduce work for each image!
+
+class RedDescriptor(DescriptorGenerator): 
+    def compute(self, input_image: Image, bins: int = 256) -> np.ndarray:
+        rgb_image = np.array(input_image)
+        histogram, _ = np.histogram(rgb_image[:, :, 0], bins=bins, range=(0, 255))
+        return histogram
+    
+class GreenDescriptor(DescriptorGenerator):
+    def compute(self, input_image: Image, bins: int = 256) -> np.ndarray:
+        rgb_image = np.array(input_image)
+        histogram, _ = np.histogram(rgb_image[:, :, 1], bins=bins, range=(0, 255)) 
+        return histogram
+    
+class BlueDescriptor(DescriptorGenerator):
+    def compute(self, input_image: Image, bins: int = 256) -> np.ndarray:
+        rgb_image = np.array(input_image)
+        histogram, _ = np.histogram(rgb_image[:, :, 2], bins=bins, range=(0, 255))
+        return histogram
+    
+# HSL
+
+class HueDescriptor(DescriptorGenerator):
+    def compute(self, input_image: Image, bins: int = 256) -> np.ndarray:
+        hsv_image = input_image.convert("HSV")
+        histogram, _ = np.histogram(np.array(hsv_image)[:, :, 0] , bins=bins, range=(0, 255))
+        return histogram
+    
+class SaturationDescriptor(DescriptorGenerator):
+    def compute(self, input_image: Image, bins: int = 256) -> np.ndarray:
+        hsv_image = input_image.convert("HSV")
+        histogram, _ = np.histogram(np.array(hsv_image)[:, :, 0] , bins=bins, range=(0, 255))
+        return histogram
+    
+class ValueDescriptor(DescriptorGenerator):
+    def compute(self, input_image: Image, bins: int = 256) -> np.ndarray:
+        hsv_image = input_image.convert("HSV")
+        histogram, _ = np.histogram(np.array(hsv_image)[:, :, 0] , bins=bins, range=(0, 255))
+        return histogram
+
+# TODO: Add CieLab, YCbCr, ...
