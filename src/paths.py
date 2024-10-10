3b08f2705d20a74f615c425560c2a85a90139a24
IararIV <gerard.jover2000@gmail.com>
2024-10-10 18:28:04 +0200

    Week 2 data

--- src/paths.py
+++ src/paths.py
@@ -21,3 +21,19 @@ assert QSD1_W1_PATH.exists()
 assert QST1_W1_PATH.exists()
 assert GT_QSD1_W1_PATH.exists()
 assert WEEK_1_RESULTS_PATH.exists()
+
+# W2
+WEEK_2_PATH = PROJECT_ROOT_PATH / "WEEK_2"
+QSD1_W2_PATH = DATA_PATH / "qsd1_w2"
+QSD2_W2_PATH = DATA_PATH / "qsd2_w2"
+GT_QSD1_W2_PATH = QSD1_W2_PATH / "gt_corresps.pkl"
+GT_QSD2_W2_PATH = QSD2_W2_PATH / "gt_corresps.pkl"
+GT_FRAMES_QSD2_W2_PATH = QSD2_W2_PATH / "gt_corresps.pkl"
+WEEK_2_RESULTS_PATH = WEEK_2_PATH / "results"
+assert WEEK_2_PATH.exists()
+assert QSD1_W2_PATH.exists()
+assert QSD2_W2_PATH.exists()
+assert GT_QSD1_W2_PATH.exists()
+assert GT_QSD2_W2_PATH.exists()
+assert GT_FRAMES_QSD2_W2_PATH.exists()
+assert WEEK_2_RESULTS_PATH.exists()
