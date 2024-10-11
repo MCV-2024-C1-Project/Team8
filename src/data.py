28b7b7df313a27ee14a87a7882bb0309f74cc1ce
AlbertRoca29 <albertrocallevadot@gmail.com>
2024-10-11 12:23:32 +0200

    Encaminat !

--- src/data.py
+++ src/data.py
@@ -1,6 +1,11 @@
 import pickle
 
 from src.paths import GT_QSD1_W1_PATH
+from src.paths import GT_QSD1_W2_PATH
 
 with GT_QSD1_W1_PATH.open('rb') as f:
     GT_QSD1_W1_LIST = pickle.load(f)
+
+
+with GT_QSD1_W2_PATH.open('rb') as f:
+    GT_QSD1_W2_LIST = pickle.load(f)
